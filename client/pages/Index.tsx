8c498191d24e6cb1a9dd531f8227ee195556511f
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-08-31 15:01:55 +0530

    Update Index.tsx

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "../components/Hero";
 import { Features } from "../components/Features";
 import { HowItWorks } from "../components/HowItWorks";
+import { TierRewards } from "../components/TierRewards";
 import { Footer } from "../components/Footer";
 import { Layout } from "../components/Layout";
 
@@ -8,12 +9,19 @@ export default function Index() {
   return (
     <Layout>
       <div className="relative">
+        {/* Hero Section */}
         <Hero />
 
+        {/* Features Section */}
         <Features />
 
+        {/* How It Works Section */}
         <HowItWorks />
 
+        {/* Tier Rewards Section */}
+        <TierRewards />
+
+        {/* Footer */}
         <Footer />
       </div>
     </Layout>
